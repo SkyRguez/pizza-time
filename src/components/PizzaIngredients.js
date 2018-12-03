6a8e824348a0550222a8a559362899e290f0bfea
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-12-03 11:14:27 +0100

    Send props to PizzaIngredients from PizzaIngredient

--- src/components/PizzaIngredients.js
+++ src/components/PizzaIngredients.js
@@ -1,3 +1,26 @@
 /* TODO: Create the PizzaIngredients Component consisting of:
   - An `ul` where you `map` over `ingredients` to create a `PizzaIngredient` per `ingredientKey`
 */
+import React from "react";
+import PizzaIngredient from "./PizzaIngredient";
+
+class PizzaIngredients extends React.Component {
+  render() {
+    return (
+      <ul>
+        {Object.keys(this.props.ingredients).map(ingredientKey => (
+          <PizzaIngredient
+            ingredients={this.props.ingredients}
+            ingredientsDetails={this.props.ingredients[ingredientKey]}
+            ingredient={this.props.ingredient}
+            addIngredient={this.props.add}
+            ingredientKey={ingredientKey}
+            key={ingredientKey}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default PizzaIngredients;
