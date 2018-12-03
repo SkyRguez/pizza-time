3ad981f64434d08053f20ad0203ab9a1759a509d
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-12-03 11:13:59 +0100

    Add PizzaIngredient component

--- src/components/PizzaIngredient.js
+++ src/components/PizzaIngredient.js
@@ -9,3 +9,31 @@
       Show the value of the current ingredient `cash` property.
       (HINT: 👆 Check out `src/helpers.js` to import the `formatPrice` function)
 */
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class PizzaIngredient extends React.Component {
+  handleClick = _event => {
+    this.props.addIngredient(this.props.ingredientKey);
+  };
+
+  render() {
+    const { name, cash } = this.props.ingredientsDetails;
+    return (
+      <li
+        className={this.props.ingredients.selected ? "marked" : ""}
+        onClick={this.handleClick}
+      >
+        <h3>
+          <span className="phrase">{name}</span>
+        </h3>
+        <h3>
+          {this.props.ingredients.cash}
+          <span className="points">{formatPrice(cash)}</span>
+        </h3>
+      </li>
+    );
+  }
+}
+
+export default PizzaIngredient;
