c890f9ceffbced3cc7093635661816011b7a69bf
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-12-03 09:00:06 +0100

    Create <PizzaHeader> component and export it

--- src/components/PizzaHeader.js
+++ src/components/PizzaHeader.js
@@ -3,3 +3,17 @@
     - An `h1` with class `pizza-text`
       Show the text `🍕 It's Pizza Time!`
 */
+
+import React from "react";
+
+class PizzaHeader extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>🍕 It's Pizza Time!</h1>
+      </header>
+    );
+  }
+}
+
+export default PizzaHeader;
